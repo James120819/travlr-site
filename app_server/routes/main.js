6c41f94f66898bb0c78b395066d1fe0250603424
James120819 <jamesfarrow1999@gmail.com>
2025-07-23 21:59:06 -0400

    Update main.js

--- app_server/routes/main.js
+++ app_server/routes/main.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-const mainController = require('../controllers/main');
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Travlr Getaways' });
+});
 
-router.get('/', mainController.index);
-router.get('/about', mainController.about);
-router.get('/contact', mainController.contact);
-router.get('/meals', mainController.meals);
-router.get('/news', mainController.news);
-router.get('/rooms', mainController.rooms);
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'About' });
+});
+
+router.get('/rooms', function (req, res) {
+    res.render('rooms', { title: 'Rooms' });
+});
+
+router.get('/meals', function (req, res) {
+    res.render('meals', { title: 'Meals' });
+});
+
+router.get('/travel', function (req, res) {
+    res.render('travel', { title: 'Travel' });
+});
+
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'Contact' });
+});
+
+router.get('/news', function (req, res) {
+    res.render('news', { title: 'News' });
+});
 
 module.exports = router;
