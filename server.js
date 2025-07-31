839c61c7dad390047551720d0887d12cd727f891
James120819 <jamesfarrow1999@gmail.com>
2025-07-31 11:00:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 const app = express();
+
 const mainRouter = require('./app_server/routes/main');
+const apiRouter = require('./app_server/routes/api');
+
+const uri = "mongodb+srv://jamesfarrow1999:Ilovemyself25@cluster0.02kzzuq.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected via Mongoose'))
+.catch(err => console.error('MongoDB connection error:', err.message));
 
 
 
@@ -14,37 +24,13 @@ app.set('view engine', 'hbs');
 hbs.registerPartials(path.join(__dirname, 'app_server', 'views', 'partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/', mainRouter);
-
-
-
-
-const uri = "mongodb+srv://jamesfarrow1999:Ilovemyself25@cluster0.02kzzuq.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-
-let db;
-
-async function connectToDB() {
-   try {
-    await client.connect();
-    db = client.db("travlr");
-    console.log("Connected to MongoDB!");
-   } catch (err) {
-      console.error("MongoDB connection error:", err);
-   }
-}
-connectToDB();
-
+app.use('/api', apiRouter);
 
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
-
-module.exports = { db };
